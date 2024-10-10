43cce983e530b82c93cb91b4b6ef439d75c033a0
Lucas Gamer Youtubes Games . com <lwemorais@gmail.com>
2024-10-10 16:29:50 -0300

    Update script.js
    carrossel concertado e autoplay desativado

--- script/script.js
+++ script/script.js
@@ -2,18 +2,18 @@ let slideIndex = 0;
 
 function showVideos() {
     const videos = document.querySelectorAll('.video');
-    const titles = document.querySelectorAll('.video-title'); // Seleciona todos os títulos
+    const titles = document.querySelectorAll('.video-title');
     videos.forEach((video, index) => {
-        // Usa a classe 'active' para mostrar o vídeo atual
+
         video.classList.toggle('active', index === slideIndex);
         
         if (index === slideIndex) {
-            video.style.display = 'block'; // Mostra o vídeo ativo
-            titles[index].style.display = 'block'; // Mostra o título correspondente
+            video.style.display = 'block';
+            titles[index].style.display = 'block';
         } else {
-            video.style.display = 'none'; // Esconde vídeos inativos
-            titles[index].style.display = 'none'; // Esconde o título correspondente
-            video.pause(); // Para os vídeos que não estão ativos
+            video.style.display = 'none'; 
+            titles[index].style.display = 'none'; 
+            video.pause(); 
         }
     });
 }
@@ -24,7 +24,7 @@ function moveCarrossel(n) {
     showVideos();
 }
 
-// Inicializa mostrando o primeiro vídeo
+
 document.addEventListener("DOMContentLoaded", () => {
     showVideos();
 });
